f35591b27a3956751f8723ba374cc8afa14ab76d
Shams Latif <54438553+shamsla@users.noreply.github.com>
2021-03-09 13:39:31 +0500

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,12 +1,3 @@
-##########################################################
-
-#              Programmer: ShAms LA
-#              GitHub: https://github.com/shams-la
-#              Email: contact.shams.in@gmail.com
-
-##########################################################
-
-
 import smtplib
 
 GETBOOL = False
